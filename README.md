4069154e15055a44e81e67f895f93edee154df67
Amio <amio.cn@gmail.com>
2019-03-13 22:54:01 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# latest-now-cli
+
+Download latest now-cli binary from CircleCI.
+
+## Usage
+
+- `/download?branch=now-dev&bin=now-alpine` Redirect to last successful built binary
+- `/download/16226/now-linux` Download `now-linux` from build #16226
---
+++ index.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const { send } = require('micro')
+const { router, get } = require('micro-fork')
+const fetch = require('node-fetch')
+
+const API_PREFIX = 'https://circleci.com/api/v1.1/project/github/zeit/now-cli'
+
+const readme = (req, res) => {
+  send(res, 200, fs.readFileSync('./README.md', 'utf8'))
+}
+
+const downloadLatestBuild = async (req, res) => {
+  const url = `${API_PREFIX}/tree/now-dev`
+  const builds = await fetch(url).then(res => res.json())
+
+  if (builds.length) {
+    const buildNum = builds[0].previous_successful_build.build_num
+    const binName = req.query.bin || 'now-linux'
+
+    res.setHeader('Location', `/download/${buildNum}/${binName}`)
+    res.setHeader('Cache-Control', 'public, s-maxage=300')
+    send(res, 302)
+  }
+
+  send(res, 404)
+}
+
+const downloadBuild = async (req, res) => {
+  const { build_num, bin_name = 'now-linux' } = req.params
+
+  res.setHeader('Cache-Control', 'public, s-maxage=604800')
+
+  if (!build_num) {
+    return send(res, 400)
+  }
+
+  const url = `${API_PREFIX}/${build_num}/artifacts`
+  const artifacts = await fetch(url).then(res => res.json())
+
+  if (artifacts instanceof Array) {
+    const found = artifacts.find(file => file.path.endsWith(bin_name))
+    if (found) {
+      console.log(`[${build_num}/${bin_name}]`, found.url)
+
+      return fetch(found.url).then(resp => {
+        resp.body.pipe(res);
+      })
+    }
+  }
+
+  console.error(`[404] ${req.url}`);
+  send(res, 404)
+}
+
+module.exports = router()(
+  get('/', readme),
+  get('/download', downloadLatestBuild),
+  get('/download/:build_num/:bin_name', downloadBuild)
+);
